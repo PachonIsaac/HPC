61f8513da3fa98c3b123283805d4570adb1bf507
JenseDM <jense.martinez@utp.edu.co>
2025-09-29 23:55:31 -0500

    Correccion generacion de benchmark

--- HPCReto1/src/pi_dartboard_serial.c
+++ HPCReto1/src/pi_dartboard_serial.c
@@ -5,20 +5,24 @@
 #include <time.h>
 #include <sys/time.h>
 
-#define DARTS 10000000
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Uso: %s <iteraciones>\n", argv[0]);
+        return 1;
+    }
+    long darts = atol(argv[1]);
     int hits = 0;
     srand(time(NULL));
     struct timeval start, end;
     gettimeofday(&start, NULL);
-    for (int i = 0; i < DARTS; i++) {
+    for (long i = 0; i < darts; i++) {
         double x = ((double)rand() / RAND_MAX) * 2.0 - 1.0;
         double y = ((double)rand() / RAND_MAX) * 2.0 - 1.0;
         if (x * x + y * y <= 1.0) hits++;
     }
     gettimeofday(&end, NULL);
-    double pi = 4.0 * ((double)hits / DARTS);
+    double pi = 4.0 * ((double)hits / darts);
     double elapsed = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1e6;
     printf("Monte Carlo Dartboard (Serial): PI estimado = %.8f\n", pi);
     printf("Tiempo de ejecución: %.6f segundos\n", elapsed);
--- HPCReto1/src/pi_needles_serial.c
+++ HPCReto1/src/pi_needles_serial.c
@@ -5,23 +5,29 @@
 #include <math.h>
 #include <time.h>
 
-#define NEEDLES 10000000
+#define LENGTH 1.0
+#define DIST 1.0
 #define LENGTH 1.0
 #define DIST 1.0
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Uso: %s <iteraciones>\n", argv[0]);
+        return 1;
+    }
+    long needles = atol(argv[1]);
     int hits = 0;
     srand(time(NULL));
     struct timeval start, end;
     gettimeofday(&start, NULL);
-    for (int i = 0; i < NEEDLES; i++) {
+    for (long i = 0; i < needles; i++) {
         double x = ((double)rand() / RAND_MAX) * (DIST / 2);
         double theta = ((double)rand() / RAND_MAX) * M_PI;
         double reach = (LENGTH / 2) * sin(theta);
         if (x <= reach) hits++;
     }
     gettimeofday(&end, NULL);
-    double pi = (2.0 * LENGTH * NEEDLES) / (DIST * hits);
+    double pi = (2.0 * LENGTH * needles) / (DIST * hits);
     double elapsed = (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1e6;
     printf("Buffon's Needle (Serial): PI estimado = %.8f\n", pi);
     printf("Tiempo de ejecución: %.6f segundos\n", elapsed);
